198089e311aa80d750a9c4c210d58d0634cb8da3
J-Stocks <36339338+J-Stocks@users.noreply.github.com>
2020-12-30 23:46:32 +0000

    Added login via GitHub using Laravel Socialite

---
+++ app/Http/Controllers/Auth/GitHubController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Laravel\Socialite\Facades\Socialite;
+
+class GitHubController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function callback()
+    {
+        $user = $this->findOrCreateUser(Socialite::driver('github')->user());
+        Auth::login($user);
+        return redirect(route('dashboard'));
+    }
+
+    public function findOrCreateUser($githubUser)
+    {
+        try {
+            $user = User::where('github_id', $githubUser->getId())->firstOrFail();
+        } catch (ModelNotFoundException $exception) {
+            $user = User::create([
+                'name' => $githubUser->getNickname(),
+                'email' => $githubUser->getEmail(),
+                'password' => Hash::make(Str::random(32)),
+                'github_id' => $githubUser->getId(),
+            ]);
+        }
+        return $user;
+    }
+}
